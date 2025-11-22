2fc5080047673a633f48c70f64a932e7b0e7804e
João Vitor <joaovitorpessoal2@gmail.com>
2025-11-22 20:47:49 -0300

    Adicao de novas funções para calcular overal para cada posição

--- FootGame/FootGame.Domain/Services/PlayerOverallCalculator.cs
+++ FootGame/FootGame.Domain/Services/PlayerOverallCalculator.cs
@@ -16,6 +16,24 @@ public class PlayerOverallCalculator
             case PlayerPosition.CenterBack:
                 weightedRating = CalculateCenterBackOverall(skill);
                 break;
+            case PlayerPosition.FullBack:
+                weightedRating = CalculateFullBackOverall(skill);
+                break;
+            case PlayerPosition.DefensiveMidfielder:
+                weightedRating = CalculateDefensiveMidfielder(skill);
+                break;
+            case PlayerPosition.CentralMidfielder:
+                weightedRating = CalculateCentralMidfielder(skill);
+                break;
+            case PlayerPosition.Winger:
+                weightedRating = CalculateWinger(skill);
+                break;
+            case PlayerPosition.SecondStriker:
+                weightedRating = CalculateSecondStriker(skill);
+                break;
+            case PlayerPosition.Striker:
+                weightedRating = CalculateStriker(skill);
+                break;
         }
 
         int finalOverall = (int)Math.Round(weightedRating);
@@ -28,8 +46,8 @@ public class PlayerOverallCalculator
                 (skill.Diving * 0.20) +
                 (skill.Handling * 0.20) +
                 (skill.GoalkeepingPositioning * 0.20) +
-                (skill.Composure * 0.05) +       
-                (skill.LongPassing * 0.05) +  
+                (skill.Composure * 0.05) +
+                (skill.LongPassing * 0.05) +
                 (skill.Jumping * 0.05);
     }
 
@@ -38,12 +56,93 @@ public class PlayerOverallCalculator
         return (skill.Marking * 0.20) +
                    (skill.Tackling * 0.20) +
                    (skill.Interception * 0.20) +
-                   (skill.DefensiveVision * 0.25) + 
+                   (skill.DefensiveVision * 0.25) +
                    (skill.Strength * 0.15) +
                    (skill.Heading * 0.10) +
                    (skill.Jumping * 0.10) +
                    (skill.Aggressiveness * 0.10) +
                    (skill.Determination * 0.05) +
-                   (skill.Passing * 0.10);   
+                   (skill.Passing * 0.10);
+    }
+
+    private double CalculateFullBackOverall(PlayerSkill skill)
+    {
+        return (skill.Pace * 0.15) +
+                (skill.Stamina * 0.15) +
+                (skill.Acceleration * 0.10) +
+                (skill.Crossing * 0.20) +
+                (skill.Tackling * 0.10) +
+                (skill.Marking * 0.10) +
+                (skill.Dribbling * 0.05) +
+                (skill.Positioning * 0.10) +
+                (skill.Passing * 0.10) +
+                (skill.Agility * 0.05);
+    }
+
+    private double CalculateDefensiveMidfielder(PlayerSkill skill)
+    {
+        return (skill.Interception * 0.20) +
+               (skill.Tackling * 0.20) +
+               (skill.Stamina * 0.15) +
+               (skill.Strength * 0.15) +
+               (skill.Passing * 0.15) +
+               (skill.LongPassing * 0.10) +
+               (skill.DefensiveVision * 0.15) +
+               (skill.Teamwork * 0.05) +
+               (skill.Aggressiveness * 0.05) +
+               (skill.Composure * 0.10);
+    }
+
+    private double CalculateCentralMidfielder(PlayerSkill skill)
+    {
+        return (skill.Passing * 0.20) +
+               (skill.Vision * 0.20) +
+               (skill.BallControl * 0.20) +
+               (skill.Dribbling * 0.10) +
+               (skill.LongPassing * 0.15) +
+               (skill.Stamina * 0.15) +
+               (skill.Composure * 0.05) +
+               (skill.LongShots * 0.10) +
+               (skill.Agility * 0.05) +
+               (skill.Teamwork * 0.15);
+    }
+
+    private double CalculateWinger(PlayerSkill skill)
+    {
+        return (skill.Pace * 0.15) +
+               (skill.Acceleration * 0.15) +
+               (skill.Dribbling * 0.20) +
+               (skill.Agility * 0.15) +
+               (skill.Crossing * 0.15) +
+               (skill.BallControl * 0.15) +
+               (skill.Finishing * 0.05) +
+               (skill.Stamina * 0.10) +
+               (skill.Vision * 0.10) +
+               (skill.Passing * 0.05);
+    }
+
+    private double CalculateSecondStriker(PlayerSkill skill)
+    {
+        return (skill.Dribbling * 0.20) +
+               (skill.Finishing * 0.15) +
+               (skill.BallControl * 0.20) +
+               (skill.Vision * 0.15) +
+               (skill.Agility * 0.10) +
+               (skill.Acceleration * 0.10) +
+               (skill.Passing * 0.10) +
+               (skill.LongShots * 0.10) +
+               (skill.Positioning * 0.10);
+    }
+
+    private double CalculateStriker(PlayerSkill skill)
+    {
+        return (skill.Finishing * 0.25) +
+               (skill.Heading * 0.20) +
+               (skill.Positioning * 0.20) +
+               (skill.ShotPower * 0.15) +
+               (skill.Strength * 0.15) +
+               (skill.Jumping * 0.15) +
+               (skill.Composure * 0.05) +
+               (skill.BallControl * 0.05);
     }
 }
