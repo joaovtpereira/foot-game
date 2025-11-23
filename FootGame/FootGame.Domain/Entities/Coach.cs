4d904e0f5df8feed5b374f1d1e8349172f3dc886
João Vitor <joaovitorpessoal2@gmail.com>
2025-11-23 16:26:51 -0300

    adicao de logica para verificar se técnico tem mais de 18 anos

--- FootGame/FootGame.Domain/Entities/Coach.cs
+++ FootGame/FootGame.Domain/Entities/Coach.cs
@@ -1,8 +1,22 @@
+using System.Security.Cryptography;
+
 namespace FootGame.Domain.Entities;
 
-public class Coach(string name, DateTime birthDate, Guid nationalityId, Guid? teamId = null): Person(name, birthDate, nationalityId)
+public class Coach: Person
 {
-    public Guid? TeamId { get; private set; } = teamId;
+    public Guid? TeamId { get; private set; }
+
+    public Coach(string name, DateTime birthDate, Guid nationalityId, Guid? teamId = null): base(name, birthDate, nationalityId)
+    {
+        int age = GetAge(DateTime.Now);
+        
+        if (age < 18)
+        {
+            throw new ArgumentException("Coach must be at least 18 years old.");
+        }
+
+        TeamId = teamId;
+    }
     
     public Coach SetTeamId(Guid teamId)
     {
