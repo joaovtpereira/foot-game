b857f1c9150432e915595b861a217896931930f2
João Vitor <joaovitorpessoal2@gmail.com>
2025-11-23 16:24:50 -0300

    Criação de classe abstrata para person

--- FootGame/FootGame.Domain/Entities/Coach.cs
+++ FootGame/FootGame.Domain/Entities/Coach.cs
@@ -1,34 +1,12 @@
 namespace FootGame.Domain.Entities;
 
-public class Coach(string name, DateTime birthDate, Guid nationalityId, Guid? teamId = null)
+public class Coach(string name, DateTime birthDate, Guid nationalityId, Guid? teamId = null): Person(name, birthDate, nationalityId)
 {
-    public Guid Id { get; private set; } = Guid.NewGuid();
-    public string Name { get; private set; } = name;
-    public DateTime BirthDate { get; private set; } = birthDate;
-    public Guid NationalityId { get; private set; } = nationalityId;
     public Guid? TeamId { get; private set; } = teamId;
-
-    public Coach SetName(string name)
-    {
-        Name = name;
-        return this;
-    }
     
     public Coach SetTeamId(Guid teamId)
     {
         TeamId = teamId;
         return this;
-    }
-
-    public int GetAge(DateTime currentDate)
-    {
-        int age = currentDate.Year - BirthDate.Year;
-
-        if (currentDate < BirthDate.AddYears(age))
-        {
-            age--;
-        }
-
-        return age;
     }
 }
--- FootGame/FootGame.Domain/Entities/Player.cs
+++ FootGame/FootGame.Domain/Entities/Player.cs
@@ -1,22 +1,12 @@
 namespace FootGame.Domain.Entities;
 using FootGame.Domain.ValueObjects;
 
-public class Player(string name, DateTime birthDate, Guid nationalityId, int height, int weight, PlayerSkill skills)
+public class Player(string name, DateTime birthDate, Guid nationalityId, int height, int weight, PlayerSkill skills): Person(name, birthDate, nationalityId)
 {
-    public Guid Id { get; private set; } = Guid.NewGuid();
-    public string Name { get; private set; } = name;
-    public DateTime BirthDate { get; private set; } = birthDate;
-    public Guid NationalityId { get; private set; } = nationalityId;
     public int Height { get; private set; } = height;
     public int Weight { get; private set; } = weight;
     public bool IsRetired { get; private set; } = false;
     public PlayerSkill Skills { get; private set; } = skills;
-
-    public Player SetName(string name)
-    {
-        Name = name;
-        return this;
-    }
 
     public Player SetHeight(int height)
     {
@@ -29,7 +19,7 @@ public class Player(string name, DateTime birthDate, Guid nationalityId, int hei
         Weight = weight;
         return this;
     }
-    
+
     public void Retire()
     {
         if (IsRetired)
